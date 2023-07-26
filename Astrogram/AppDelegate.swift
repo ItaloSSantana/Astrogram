eb6330bce5a9661c9a1cad297942a3602be5a84f
ItaloSSantana <110575556+ItaloSSantana@users.noreply.github.com>
2023-07-26 11:49:41 -0700

    update commit

--- Astrogram/AppDelegate.swift
+++ Astrogram/AppDelegate.swift
@@ -7,19 +7,29 @@
 
 import UIKit
 import CoreData
+import FirebaseCore
+import GoogleSignIn
+import FirebaseAuth
+import FacebookCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
+  
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
+    }
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
